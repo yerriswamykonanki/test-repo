c0f2345154ceb6b57fa01cea509d37cf4efc6759
Ubuntu <ubuntu@ip-172-31-10-253.ap-northeast-1.compute.internal>
2017-12-26 12:17:39 +0000

    latestcommit

--- RFW/run.sh
+++ RFW/run.sh
@@ -7,21 +7,21 @@
 #        echo connected;
 #sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
 #!/bin/bash
-count=0
-while ! nc -z cpservice 8080 
-do
- if [ $count -le 180 ] 
- then
-  echo sleeping
-  sleep 5
-  count=`expr $count + 1`
- fi
-done
-if [ $count -le 180 ]; then
+#count=0
+#while ! nc -z cpservice 8080 
+#do
+# if [ $count -le 180 ] 
+# then
+#  echo sleeping
+#  sleep 5
+#  count=`expr $count + 1`
+# fi
+#done
+#if [ $count -le 180 ]; then
    sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
-else
-   exit 1
-fi
+#else
+#   exit 1
+#fi
 #while true
 #do
 #	echo "Press [CTRL+C] to stop.."
